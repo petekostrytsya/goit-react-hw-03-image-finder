36bb6267be31413bd5737ac8c5ecf99bda96c2df
petekostrytsya <petekostrytsya@gmail.com>
2023-05-30 20:50:32 +0300

    Loader to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,7 @@ import { Loader } from './Loader/Loader';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    
+    <Loader/>
   );
 };
