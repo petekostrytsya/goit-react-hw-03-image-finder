b21eb6fc507ed00fb911b0b70c1701b5cd5d18ed
petekostrytsya <petekostrytsya@gmail.com>
2023-06-01 18:20:23 +0300

    add Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Searchbar';
 
 export const App = () => {
   return (
-    <Button/>,
+    <Searchbar/>
 
-    <Loader/>
+    
 
   );
 };
